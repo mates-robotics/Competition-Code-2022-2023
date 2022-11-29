180ee3ae81ad348fd091ff35a8fe19fe5786aeaf
Aidan Henbest <henbestaj@gmail.com>
2022-11-29 15:30:11 -0500

    Make edits

--- AutoBlueLeft.java
+++ AutoBlueLeft.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.robot.Robot;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
@@ -13,16 +14,13 @@ import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 
 @Autonomous(name = "AutoBlueLeft")
+
 public class AutoBlueLeft extends LinearOpMode{
 	
 	RobotMap robot = new RobotMap();
 	
-	//Right side of robot starts on third line from start
 	// Establishes the "runtime" object from the "ElapsedTime" class
 	private ElapsedTime runtime = new ElapsedTime();
-	
-	
-	
 	
 	@Override
 	public void runOpMode() {
@@ -33,28 +31,18 @@ public class AutoBlueLeft extends LinearOpMode{
 		// Wait for driver to hit PLAY
 		waitForStart();
 		
-		
-		//drive to high junction
-		// methods.drive(12, 537); // works
-		
-		// testing
-		methods.turn(-90, 537);
-		
-		
-		//place cone on high junction
-		// methods.claw(4);
-		//parks robot in alliance terminal
-		//methods.strafe(30);
-		// methods.drive(-12, 537);
-		
- 		//strafes into alliance terminal (from start)
- 		// methods.strafe(36);
- 		runtime.reset();
+		methods.strafe(33, 537);
+		methods.drive(31, 537);
+		methods.extend(4);
+		methods.drive(2, 537);
+		methods.extend_precise(-200);
+		methods.claw(true);
+		methods.drive(-33, 537);
+		methods.extend(0);
 		
  		robot.leftFrontDrive.setPower(0);
  		robot.leftBackDrive.setPower(0);
  		robot.rightFrontDrive.setPower(0);
  		robot.rightBackDrive.setPower(0);
  	}
-	
  }
