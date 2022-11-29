2d8a93c738186c7e11a60cb0f73747598633ed4b
Aidan Henbest <henbestaj@gmail.com>
2022-11-29 14:06:08 -0500

    Include competiton quick edits

--- AutoBlueLeft.java
+++ AutoBlueLeft.java
@@ -17,8 +17,12 @@ public class AutoBlueLeft extends LinearOpMode{
 	
 	RobotMap robot = new RobotMap();
 	
+	//Right side of robot starts on third line from start
 	// Establishes the "runtime" object from the "ElapsedTime" class
 	private ElapsedTime runtime = new ElapsedTime();
+	
+	
+	
 	
 	@Override
 	public void runOpMode() {
@@ -29,19 +33,28 @@ public class AutoBlueLeft extends LinearOpMode{
 		// Wait for driver to hit PLAY
 		waitForStart();
 		
-		methods.strafe(33, 537);
-		methods.drive(31, 537);
-		methods.extend(4);
-		methods.drive(2, 537);
-		methods.extend(true, -200);
-		methods.claw(true);
-		methods.extend(true, 200);
-		methods.drive(-33, 537);
-		methods.extend(0);
+		
+		//drive to high junction
+		// methods.drive(12, 537); // works
+		
+		// testing
+		methods.turn(-90, 537);
+		
+		
+		//place cone on high junction
+		// methods.claw(4);
+		//parks robot in alliance terminal
+		//methods.strafe(30);
+		// methods.drive(-12, 537);
+		
+ 		//strafes into alliance terminal (from start)
+ 		// methods.strafe(36);
+ 		runtime.reset();
 		
  		robot.leftFrontDrive.setPower(0);
  		robot.leftBackDrive.setPower(0);
  		robot.rightFrontDrive.setPower(0);
  		robot.rightBackDrive.setPower(0);
  	}
+	
  }
