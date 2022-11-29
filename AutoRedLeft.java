7c1253433a7498e2e7d1f962ce1ed4fa7a612e03
Aidan Henbest <henbestaj@gmail.com>
2022-11-29 15:30:55 -0500

    Make edits

--- AutoRedLeft.java
+++ AutoRedLeft.java
@@ -1,16 +1,25 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.robot.Robot;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.Range;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 
-@Autonomous
+@Autonomous(name = "AutoRedLeft")
 
 public class AutoRedLeft extends LinearOpMode{
 	
 	RobotMap robot = new RobotMap();
+	
+	// Establishes the "runtime" object from the "ElapsedTime" class
 	private ElapsedTime runtime = new ElapsedTime();
 
 	@Override
@@ -20,20 +29,16 @@ public class AutoRedLeft extends LinearOpMode{
 		Methods methods = new Methods(robot);
 		// Wait for driver to hit PLAY
 		waitForStart();
-		//drive to high junction
-		methods.drive(12, 537);
-		methods.strafe(6, 537);
-		//place cone on high junction
-		// methods.claw(4);
-		//parks robot in alliance terminal
-		methods.strafe(-30, 537);
-		methods.drive(-12, 537);
 		
-		//strafes into alliance terminal
-		// methods.strafe(-24);
-		runtime.reset();
-		
-		
+		methods.strafe(33, 537);
+		methods.drive(31, 537);
+		methods.extend(4);
+		methods.drive(2, 537);
+		methods.extend_precise(-200);
+		methods.claw(true);
+		methods.drive(-33, 537);
+		methods.strafe(-63, 537);
+		methods.extend(0);
 		
  		robot.leftFrontDrive.setPower(0);
  		robot.leftBackDrive.setPower(0);
